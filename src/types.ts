0d0124391048fe4c6cf7b04878f5364f5133934a
Moses <159237123+mmugo79@users.noreply.github.com>
2026-06-29 21:03:20 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -12,7 +12,7 @@ export interface User {
   email: string;
   role: 'CITIZEN' | 'ADMIN' | 'OFFICIAL';
   avatar?: string;
-  county?: string;
+  county?: string; 
   assignedCategories?: IssueCategory[];
   institution?: string;
   phone?: string;
