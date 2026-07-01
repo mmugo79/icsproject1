7b2ada6485722dbe0d7a6b5a528d3d88759ced61
Moses <159237123+mmugo79@users.noreply.github.com>
2026-07-01 22:58:40 +0300

    Update postgres.ts

--- src/lib/postgres.ts
+++ src/lib/postgres.ts
@@ -10,7 +10,7 @@ export interface PGUser {
   full_name: string;
   email: string;
   password_hash: string;
-  phone_number: string;
+  phone_number: string; 
   age?: number;
   county: string;
   constituency: string;
