5645022fbff93be989f092a315341de84acf8109
Alfred Kilonzo <alfredkilonzo90@gmail.com>
2026-07-01 10:27:05 +0300

    debugging

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 // @ts-ignore: allow side-effect import of CSS without explicit type declarations
-import './index.css' ;
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
